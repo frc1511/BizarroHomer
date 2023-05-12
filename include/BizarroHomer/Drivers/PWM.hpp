8f76eadd8b27db8c321113fb240061e9aca44e51
Peter Lilley <71025767+PeteLilley@users.noreply.github.com>
2023-05-12 18:03:12 -0400

    Created generic PWM driver

---
+++ include/BizarroHomer/Drivers/PWM.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <string>
+#include <filesystem>
+
+class PWM {
+public:
+  /**
+   * @brief Creates a PWM class instance.
+   *
+   * @param channel The PWM channel to use.
+   */
+  PWM(int channel);
+  
+  virtual ~PWM() = default;
+  
+  /**
+   * @brief Sets the duty cycle of the PWM channel.
+   *
+   * @param duty_cycle The duty cycle (ns).
+   */
+  void set_duty_cycle(int duty_cycle);
+  
+  /**
+   * @brief Sets the period of the PWM channel.
+   *
+   * @param period The period (ns).
+   */
+  void set_period(int period);
+
+  /**
+   * @brief Enables/Disables the PWM channel.
+   *
+   * @param enbaled Whether the PWM channel is enabled.
+   */
+  void set_enabled(bool enabled);
+  
+private:
+  int channel;
+  
+  std::filesystem::path channel_dir;
+  std::filesystem::path enable_path, period_path, duty_cycle_path;
+};
---
+++ src/Drivers/PWM.cpp
@@ -0,0 +1,37 @@
+#include <BizarroHomer/Drivers/PWM.hpp>
+#include <fmt/core.h>
+#include <string>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+PWM::PWM(int _channel)
+: channel(_channel),
+  channel_dir(fmt::format("/sys/class/pwm/pwmchip0/pwm{}", channel)),
+  enable_path(channel_dir / "enable"),
+  period_path(channel_dir / "period"),
+  duty_cycle_path(channel_dir / "duty_cycle") { }
+
+void PWM::set_duty_cycle(int duty_cycle) {
+  std::string msg(std::to_string(duty_cycle));
+  
+  int duty_cycle_fd = open(duty_cycle_path.c_str(), O_WRONLY | O_CREAT);
+  write(duty_cycle_fd, msg.c_str(), msg.length());
+  close(duty_cycle_fd);
+}
+
+void PWM::set_period(int period) {
+  std::string msg(std::to_string(period));
+  
+  int period_fd = open(period_path.c_str(), O_WRONLY | O_CREAT);
+  write(period_fd, msg.c_str(), msg.length());
+  close(period_fd);
+}
+
+void PWM::set_enabled(bool enabled) {
+  std::string msg(std::to_string(static_cast<int>(enabled)));
+  
+  int enable_fd = open(enable_path.c_str(), O_WRONLY | O_CREAT);
+  write(enable_fd, msg.c_str(), msg.length());
+  close(enable_fd);
+}
