3e5eb8f94e73a2ba6b78ff78319df86dfa6dc601
Peter Lilley <71025767+PeteLilley@users.noreply.github.com>
2023-08-02 15:41:12 -0400

    Comments

--- main/include/BizarroHomer/Util/Song.hpp
+++ main/include/BizarroHomer/Util/Song.hpp
@@ -5,11 +5,24 @@
 
 using namespace std::literals::chrono_literals;
 
+//
+// Represents a song to be played by Falcon 500s.
+//
 struct Song {
+  // The .chrp files to play on the motors.
   std::filesystem::path primary_chrp;
   std::filesystem::path secondary_chrp;
+  
+  // The some length (ms).
   std::chrono::milliseconds length;
   
+  //
+  // Constructs a song from a single music file.
+  //
   Song(std::filesystem::path primary_chrp, std::chrono::milliseconds length);
+  
+  //
+  // Constructs a song from a primary and a secondary music file.
+  //
   Song(std::filesystem::path primary_chrp, std::filesystem::path secondary_chrp, std::chrono::milliseconds length);
 };
